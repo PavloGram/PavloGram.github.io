5599c989e37b5199b2678f28181e556724c30937
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-09-04 10:50:29 +0300

    add react router library

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import React from 'react'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
