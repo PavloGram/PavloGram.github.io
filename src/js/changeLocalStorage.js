84e50c3b0cab0b4033c4bce984da0ba74da32ac6
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-11-02 17:17:56 +0200

    add redirect

--- src/js/changeLocalStorage.js
+++ src/js/changeLocalStorage.js
@@ -7,7 +7,8 @@ export function changeLocalStorage(
   storageKey
 ) {
   const currentFilmId = currentFilm.id ;
-  let arrey = [...localStorageIdArrey ]|| [];
+  let arrey = [...localStorageIdArrey ];
+  // console.log(arrey)
   if (arrey.includes(currentFilmId)) {
     arrey.splice(localStorageIdArrey.indexOf(currentFilmId), 1);
     localStorage.setItem(storageKey, JSON.stringify(arrey));
